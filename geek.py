bee3c9778c1c74dc9a0e7243726261711aa5607f
Caique-beep <caiquecsoares3@gmail.com>
2020-09-24 09:54:35 -0300

    mais um execicio

--- geek.py
+++ geek.py
@@ -1,17 +1,23 @@
+"""
+Given an integer,n , perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+"""
+
+
 def weird(n):
-    if 1 <= n and n >=100:
-        if n % 2 == 0:
-            return print("weird")
-        elif n in range(2,6):
-            return print("not weird")
-        elif n in range(6,20):
+    if n in range(1,100):
+        if n % 2 == 1:
             return print("Weird")
-        elif n >= 20:
-            return print("Not weird")
-    else:
-        return print("O numero tem que ser entre 1 e 100")
-
-
+        if n % 2 == 0 and n in range(2, 6) or n >= 20:
+            return print("Not Weird")
+        if n % 2 == 0 and n in range(6, 20):
+            return print("Weird")
+        else:
+            return print("O numero tem que ser entre 1 e 100")
 
 
 weird(3)
